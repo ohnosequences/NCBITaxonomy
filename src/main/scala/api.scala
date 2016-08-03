0b72e7742b7200fe1573fb2859c98a57d2bd06ab
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-08-03 16:44:44 +0200

    add original tests from mg7

--- src/main/scala/api.scala
+++ src/main/scala/api.scala
@@ -17,11 +17,44 @@ case object api {
     def rankName: String
     /* Note that this is an option because the root has no parent */
     def   parent: Option[V]
-    def rankNumber: Int
+    def rankNumber(implicit conv: V => Taxon[V]): Int =
+      rankName.trim.toLowerCase match {
+        case "superkingdom"     => 1
+        case "kingdom"          => 2
+        case "superphylum"      => 3
+        case "phylum"           => 4
+        case "subphylum"        => 5
+        case "class"            => 6
+        case "subclass"         => 7
+        case "order"            => 8
+        case "suborder"         => 9
+        case "family"           => 10
+        case "subfamily"        => 11
+        case "tribe"            => 12
+        case "subtribe"         => 13
+        case "genus"            => 14
+        case "subgenus"         => 15
+        case "species group"    => 16
+        case "species subgroup" => 17
+        case "species"          => 18
+        case "subspecies"       => 19
+        // "no rank"
+        case _                  => parent.fold(0){ _.rankNumber } + 1
+      }
 
-    final def rank: String = s"${this.rankNumber}: ${this.rankName}"
+    final def rank(implicit conv: V => Taxon[V]): String = s"${this.rankNumber}: ${this.rankName}"
 
-    def ancestors: Seq[V]
+    def ancestors(implicit conv: V => Taxon[V]): Seq[V] = {
+
+      @annotation.tailrec
+      def ancestors_rec(n: V, acc: Seq[V]): Seq[V] =
+        n.parent match {
+          case None     => n +: acc
+          case Some(p)  => ancestors_rec(p, n +: acc)
+        }
+
+      ancestors_rec(v, Seq())
+    }
   }
 
   implicit final class Taxa[V](val nodes: Traversable[V]) extends AnyVal {
---
+++ src/test/scala/dummyTree.scala
@@ -0,0 +1,58 @@
+package ohnosequences.ncbitaxonomy.test
+
+import ohnosequences.ncbitaxonomy._, api._
+
+case object dummy {
+
+  implicit final class NodeOps(val v: Node) extends Taxon[Node] {
+
+    def id = v.id
+    def name = v.name
+    def rankName = v.rankName
+
+    def parent = v.parent
+  }
+
+  sealed trait Node {
+
+    def v = this
+    def id = this.toString
+    def name = id
+    def rankName = ""
+    def parent: Option[Node]
+  }
+
+  class child(val p: Node) extends Node {
+
+    def parent = Some(p)
+  }
+
+  case object root extends Node {
+
+    def parent = None
+  }
+
+  // // common part
+  lazy val c1 = new child(root)
+  lazy val c2 = new child(c1)
+  // left branch
+  lazy val l1 = new child(c2)
+  lazy val l2 = new child(l1)
+  // right branch
+  lazy val r1 = new child(c2)
+  lazy val r2 = new child(r1)
+  lazy val r3 = new child(r2)
+
+  val common = Seq(root, c1, c2)
+
+  val allNodes: Set[Node] = Set(root, c1, c2, l1, l2, r1, r2, r3)
+
+  val id2node: Map[String, Node] = allNodes.map{ n => (n.id -> n) }.toMap
+
+  case object dummyGraph extends TaxonomyGraph[Node] {
+
+    def getTaxon(id: String): Option[Node] = id2node.get(id)
+    def root: Node = dummy.root
+  }
+
+}
