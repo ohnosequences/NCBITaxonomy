462da70efa78a914f27e6616c7a58fbea0ebbc03
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2018-03-28 11:38:16 +0200

    Remove titan-related code

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,19 +1,3 @@
 package ohnosequences
 
-import com.thinkaurelius.titan.core._, schema._
-import com.bio4j.titan.util.DefaultTitanGraph
-
-package object ncbitaxonomy {
-
-  type TitanNode =
-    com.bio4j.model.ncbiTaxonomy.vertices.NCBITaxon[
-      DefaultTitanGraph,
-      TitanVertex,
-      VertexLabelMaker,
-      TitanEdge,
-      EdgeLabelMaker
-    ]
-
-  final def optional[T](jopt: java.util.Optional[T]): Option[T] =
-    if (jopt.isPresent) Some(jopt.get) else None
-}
+package object ncbitaxonomy {}
