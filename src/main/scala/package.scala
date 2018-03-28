8ad0b3963b08b1b398478891698d7c5b7abfbc1b
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2018-03-28 15:37:55 +0200

    Add function to classify nodes scientific names

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,3 +1,25 @@
-package ohnosequences
+package ohnosequences.db
 
-package object ncbitaxonomy {}
+import ohnosequences.api.ncbitaxonomy.ScientificName
+
+package object ncbitaxonomy {
+  sealed trait NameType
+
+  case object NameType {
+    case object Normal              extends NameType
+    case object EnvironmentalSample extends NameType
+    case object Unclassified        extends NameType
+
+    def get: ScientificName => NameType =
+      scientificName => {
+        val name = scientificName.name.toLowerCase
+
+        if (name contains "unclassified")
+          NameType.Unclassified
+        else if (name startsWith "environmental samples")
+          NameType.EnvironmentalSample
+        else
+          NameType.Normal
+      }
+  }
+}
