7ceaeea05b8b852d9e7bdc286fd4a82315474087
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2018-05-18 17:55:14 +0200

    Add values for S3 storage

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -6,4 +6,25 @@ package object taxonomy {
 
   type ScientificName =
     String
+
+  val version: String =
+    "0.2.0"
+
+  val s3Prefix: S3Folder =
+    s3"resources.ohnosequences.com" /
+      "db" /
+      "taxonomy" /
+      version /
+
+  val fullTree: S3Object =
+    s3Prefix / "full.tree"
+
+  val environmentalTree: S3Object =
+    s3Prefix / "environmental.tree"
+
+  val unclassifiedTree: S3Object =
+    s3Prefix / "unclassified.tree"
+
+  val classifiedTree: S3Object =
+    s3Prefix / "classified.tree"
 }
